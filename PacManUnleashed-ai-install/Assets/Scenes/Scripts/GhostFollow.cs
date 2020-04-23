5818de349b8321e83b872d505f491ecf52dbc55b
Etoebben <ejtn78@mail.missouri.edu>
2020-04-23 16:10:01 -0500

    New Version PacMan Unleashed
    now with ai

---
+++ PacManUnleashed-ai-install/Assets/Scenes/Scripts/GhostFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GhostFollow : MonoBehaviour
+{
+    GameObject player;
+     NavMeshAgent enemy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Pacman");
+        enemy = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemy.destination=player.transform.position;
+    }
+}
