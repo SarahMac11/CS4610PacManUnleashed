5f3c4ff21c84379c686265ab9e6825a95a0befc2
Etoebben <ejtn78@mail.missouri.edu>
2020-04-25 03:04:28 -0500

    Pacman Unleashed 4-25-20
    combined scared ghost and pacdots needs work on the scared ghost transition

---
+++ PacManUnleashed-4-25-20/Assets/Scripts/BigPacdot.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigPacdot : MonoBehaviour
+{
+    void OnTriggerEnter(Collider co) {
+        if(co.gameObject.name == "Pacman") {   // if pacman collides into pac dot
+        
+        Score.score += 20;
+        Destroy(this.gameObject);    // destory pac dot game object
+        }
+    }
+}
