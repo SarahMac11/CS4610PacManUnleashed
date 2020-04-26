739ccc4606adfaf08ba9b2429cbd0aa43007c938
Tiffany Young <1tiffanycarissa1@gmail.com>
2020-04-26 03:12:05 -0500

    When scared, ghosts run away from pacman. Also, when scared pacman can eat ghost and they will respawn to box.

---
+++ PacManUnleashed-4-25-20d/Assets/Scripts/BigPacdot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigPacdot : MonoBehaviour
+{
+    void OnTriggerEnter(Collider co) {
+        if(co.gameObject.name == "Pacman") {   // if pacman collides into pac dot
+        
+        //Used to change ghosts to scared mode
+          GameObject[] ghosts =GameObject.FindGameObjectsWithTag("Enemy");
+        
+          foreach (GameObject go in ghosts)
+          {
+            go.GetComponent<GhostFollow> ().startScaredGhost ();
+          }
+           
+
+
+
+
+        Score.score += 20;
+        Destroy(this.gameObject);    // destory pac dot game object
+        }
+    }
+}
---
+++ PacManUnleashed-4-25-20d/Assets/Scripts/PManControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PManControl : MonoBehaviour
+{
+
+    public float moveSpeed;
+    private Vector3 moveDirection;
+    // Start is called before the first frame update
+    public CharacterController controller;
+    void Start(){
+        controller = GetComponent<CharacterController>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //moveDirection=(transform.foward * Input.GetAxis("Vertical"));
+        moveDirection = new Vector3(Input.GetAxis("Horizontal")*moveSpeed, 0f, Input.GetAxis("Vertical")*moveSpeed);
+        controller.Move(moveDirection * Time.deltaTime);
+        moveDirection.y = 0f;
+    }
+}
