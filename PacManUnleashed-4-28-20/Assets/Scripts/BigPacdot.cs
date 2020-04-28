203180bedc4c06e5839f32c7ef7d3ce31d72bce3
Etoebben <ejtn78@mail.missouri.edu>
2020-04-28 00:43:14 -0500

    chomp noise
    fixed the chomp noise to be more pleasing

---
+++ PacManUnleashed-4-28-20/Assets/Scripts/BigPacdot.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigPacdot : MonoBehaviour
+{
+    // get audio source
+    public AudioSource chomp;
+    
+    void Start() 
+    {
+        chomp = GetComponent<AudioSource> ();
+        // get slider volume level if set
+        chomp.volume = PlayerPrefs.GetFloat("SliderVolumeLevel", chomp.volume);
+    }
+    
+    void OnTriggerEnter(Collider co) {
+        
+        if(co.gameObject.name == "Pacman") {   // if pacman collides into pac dot
+        
+        //Used to change ghosts to scared mode
+          GameObject[] ghosts =GameObject.FindGameObjectsWithTag("Enemy");
+        
+          foreach (GameObject go in ghosts)
+          {
+            go.GetComponent<GhostFollow> ().startScaredGhost ();
+          }
+          
+        chomp.Play();
+        Score.score += 20;
+        this.gameObject.SetActive(false);   // destory pac dot game object
+        }
+    }
+}
