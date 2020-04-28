e3917d3040620b6e3cebfc8b62609c0d012f44bb
Nicolas Henry <47093382+ElBureeto@users.noreply.github.com>
2020-04-27 19:17:49 -0500

    Pacman Reset Files

--- GhostFollow.cs
+++ GhostFollow.cs
@@ -17,67 +17,79 @@ public class GhostFollow : MonoBehaviour
     public RuntimeAnimatorController blinkingGhost;
 
     //Timer Variables
-    public int scaredModeDuration= 10;
-    public int startBlinking=7;
+    public int scaredModeDuration = 10;
+    public int startBlinking = 7;
     private float scaredModeTimer = 0;
-    private float blinkTimer=0;
-    public int blink=1;
+    private float blinkTimer = 0;
+    public int blink = 1;
 
-    public static int ghostMode=1;
+    public static int ghostMode = 1;
 
     public Vector3 offset;
     public float distance;
 
     private Vector3 initialPosition = Vector3.zero;
-    
+
     // eat ghost audio
     public AudioSource eatGhost;
 
     //Ghosts Modes for Animation
-    public enum Mode {
+    public enum Mode
+    {
         Scared,
         Normal
     }
 
-    Mode currentMode=Mode.Normal;
+    Mode currentMode = Mode.Normal;
 
     void Start()
     {
         // Get audio
-        eatGhost = GetComponent<AudioSource> ();
+        eatGhost = GetComponent<AudioSource>();
         // get slider volume level if set
         eatGhost.volume = PlayerPrefs.GetFloat("SliderVolumeLevel", eatGhost.volume);
-        
+
         player = GameObject.FindGameObjectWithTag("Pacman");
         enemy = GetComponent<NavMeshAgent>();
-        spawn= GameObject.FindGameObjectWithTag("Spawn");
+        spawn = GameObject.FindGameObjectWithTag("Spawn");
 
         initialPosition = transform.position;
-        
+
         UpdateAnimatorController();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (PacmanController.pacdotCounter == 0)
+        {
+            transform.position = spawn.transform.position;
+        }
+
         float distance_between = Vector3.Distance(enemy.transform.position, player.transform.position);
-        if (currentMode== Mode.Scared)
+
+
+        if (currentMode == Mode.Scared)
         {
             //slightly slow down ghosts and run away from pacman
             GetComponent<NavMeshAgent>().speed = 4;
-            enemy.destination=player.transform.position+(transform.position-player.transform.position)*2;
-            ghostMode=0;
+            enemy.destination = player.transform.position + (transform.position - player.transform.position) * 2;
+            ghostMode = 0;
         }
-        
-        if (currentMode!= Mode.Scared){
+
+        if (currentMode != Mode.Scared)
+        {
             //Normal.Mode follow Pacman
-            if(distance_between<distance){
-            enemy.destination=player.transform.position;
-            }else{
-            enemy.destination = player.transform.position + offset;
+            if (distance_between < distance)
+            {
+                enemy.destination = player.transform.position;
+            }
+            else
+            {
+                enemy.destination = player.transform.position + offset;
             }
             GetComponent<NavMeshAgent>().speed = 7;
-            ghostMode=1;
+            ghostMode = 1;
 
         }
 
@@ -89,55 +101,56 @@ public class GhostFollow : MonoBehaviour
 
 
 
-    void UpdateAnimatorController (){
-        if (currentMode== Mode.Scared)
+    void UpdateAnimatorController()
+    {
+        if (currentMode == Mode.Scared)
         {
-            if(blink==1)
+            if (blink == 1)
             {
-             transform.GetComponent<Animator> ().runtimeAnimatorController= scaredGhost;
+                transform.GetComponent<Animator>().runtimeAnimatorController = scaredGhost;
             }
-   
+
             scaredModeTimer += Time.deltaTime;
 
-            if(scaredModeTimer >= startBlinking) //Blinking not working.....
+            if (scaredModeTimer >= startBlinking) //Blinking not working.....
             {
-                
-                if(blink==1)
+
+                if (blink == 1)
                 {
-                    blink=0;
-                    transform.GetComponent<Animator> ().runtimeAnimatorController= blinkingGhost;
+                    blink = 0;
+                    transform.GetComponent<Animator>().runtimeAnimatorController = blinkingGhost;
                 }
 
                 blinkTimer += Time.deltaTime;
-                
+
             }
 
 
             if (scaredModeTimer >= scaredModeDuration)
             {
-                scaredModeTimer=0;
+                scaredModeTimer = 0;
                 changeMode(Mode.Normal);
 
-               
+
             }
         }
         else
         {
-            transform.GetComponent<Animator> ().runtimeAnimatorController= normalGhost;
-            blink=1;
+            transform.GetComponent<Animator>().runtimeAnimatorController = normalGhost;
+            blink = 1;
         }
     }
 
 
-//Change mode of Ghosts
-    void changeMode (Mode a)
+    //Change mode of Ghosts
+    void changeMode(Mode a)
     {
-        currentMode=a;
-        UpdateAnimatorController ();
+        currentMode = a;
+        UpdateAnimatorController();
     }
 
 
-    public void startScaredGhost ()
+    public void startScaredGhost()
     {
         changeMode(Mode.Scared);
 
@@ -146,9 +159,9 @@ public class GhostFollow : MonoBehaviour
     void OnTriggerEnter(Collider other)
     {
         // if collision with scared ghost
-        if(ghostMode==0)
+        if (ghostMode == 0)
         {
-            if(other.CompareTag("Pacman"))
+            if (other.CompareTag("Pacman"))
             {
                 transform.position = spawn.transform.position;
                 // increment score when run into ghost
@@ -159,7 +172,7 @@ public class GhostFollow : MonoBehaviour
                 eatGhost.Play();
             }
         }
-         
+
     }
 
 
---
+++ Pacdot.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pacdot : MonoBehaviour
+{
+    // audio source
+    //    public AudioSource chomp;
+   // public int pacdotCounter; 
+    void Start() 
+    {
+        // Get audio
+//        chomp = GetComponent<AudioSource> ();
+    }
+    void Update()
+    {
+        if(PacmanController.pacdotCounter == 280)
+        {
+           // Debug.Log("made it");
+          //  Pacdot.SetActive(true);
+        }
+       // Debug.Log("test");
+      //  this.gameObject.SetActive(true);
+    }
+    
+
+    public void Reset()
+    {
+        //this.gameObject.SetActive(true);
+    }
+
+    void OnTriggerEnter(Collider co) {
+        if(co.gameObject.name == "Pacman") {   // if pacman collides into pac dot
+        
+//            chomp.Play();
+            // send react message
+            Score.score += 10;
+            this.gameObject.SetActive(false);
+            // destory pac dot game object
+        }
+    }
+    
+}
